5e260ce794b3027cc8a2c6e2cb84acce6ed50507
Felix <segunfelix1@yahoo.com>
2022-07-03 16:01:10 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,7 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++) putchar(letter);
-
-	putchar('\n');
+	for (letter = 'a'; letter <= 'z'; letter++) putchar(letter); putchar('\n');
 
 	return (0);
 }
