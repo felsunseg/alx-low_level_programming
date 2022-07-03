a6c5e70468a85d73caf46a208fc6221be65f6ed6
Felix <segunfelix1@yahoo.com>
2022-07-03 08:26:14 -0700

    0x0 C script

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Task 1 print the last digit of the nukmber stored in the variable
+ * main - Task 1 print the last digit of the nukmber stored in the variable n
  *
  * Return: 0
  */
@@ -15,17 +15,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	printf("%s %d is %d and is :, last, n, n % 10);
+	printf("%s %d is %d and is ", last, n, n % 10);
 	if (n % 10 > 5)
 	{
-		printf("greater than 5\n");
+	printf("greater than 5\n");
 	}
 	else if (n % 10 == 0)
-	{printf("0\n");
+	{
+	printf("0\n");
 	}
 	else
 	{
-		printif("less than 6 and not 0\n");
+	printif("less than 6 and not 0\n");
 	}
 	return (0);
 }
