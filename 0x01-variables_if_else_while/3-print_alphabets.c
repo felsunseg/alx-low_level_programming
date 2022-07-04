dd3065f075d0a4b7a2eabfc53c8fff7c45069843
Felix <segunfelix1@yahoo.com>
2022-07-03 17:02:01 -0700

    ilphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char letter;
 
-	for (le     = 'a'; lett++ <= 'z'; letter++)
+	for (le = 'a'; lett++ <= 'z'; letter++)
 		 putchar(letter);
 	
 	for (letter = 'A'; letter <= 'Z'; letter++)
