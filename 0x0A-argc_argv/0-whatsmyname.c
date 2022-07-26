e03646b56430048d26e9d539a5de0739a2241192
Felix <segunfelix1@yahoo.com>
2022-07-26 09:01:55 -0700

    hot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
  /**
- * main - prints the name of the program
- * @argc: number of arguments passed to the function
- * @argv: argument vector of pointers to strings
- *
- * Return: always 0
+ * main - prints name of file
+ * @argc: number of arguments
+ * @argv: arguments entered
+ * Description: print name of file
+ * Return: zero
  */
-int main(int __attribute__((unused)), char *argv[])
+
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	argc = argc;
 
 	return (0);
 }
