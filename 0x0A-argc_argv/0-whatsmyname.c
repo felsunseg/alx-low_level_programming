ab01e8bf25dace158f082f63cec13bd1c4feec66
Felix <segunfelix1@yahoo.com>
2022-07-25 22:40:12 -0700

    my new name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,8 @@
+/*
+ * File: 0-whatsmyname.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 
  /**
@@ -10,5 +15,6 @@
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
+
 	return(0);
 }
