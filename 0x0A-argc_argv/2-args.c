1174b7ace84f27ffafb8bca3a6e0a890de99dcf1
Felix <segunfelix1@yahoo.com>
2022-07-26 09:54:39 -0700

    hot fix

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,7 +3,7 @@
 /**
  * main - prints all arguments it recieves
  * @argc: number of arguments passed to the function
- * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
  *
  * Return: always 0
  */
