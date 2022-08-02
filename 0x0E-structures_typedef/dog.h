82edd14727031a06ffe7124a7467c203331f45ec
felsunseg <106780144+felsunseg@users.noreply.github.com>
2022-08-02 23:18:57 +0100

    hot fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,11 @@
-#ifndef _DOG_H
-#define _DOG_H
-
-#include "holberton.h"
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
- * struct dog - info on dog.
- * @name: dog's name.
- * @age: age of the dog in float.
- * @owner: name of the owner of the dog.
- *
+ * struct dog - information about dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  */
 struct dog
 {
@@ -17,11 +14,15 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-/* function for typedef dog_t */
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+
+#endif /* _DOG_H_ */
